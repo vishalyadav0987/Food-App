33f21172bae8752c711500c1ec70c6704e613cf1
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-01 13:13:39 +0530

    Hero section component

---
+++ Frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <>
+      <section id="hero-section">
+        <div className="hero">
+            <div className="content">
+            <h1>Order your favourite food</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, illo animi. Eaque possimus ipsum deserunt officiis, assumenda iste cumque aut. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <button className='btn'>View more</button>
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Hero
