8541960806522a98c1a903fcc12c9a2a6625b723
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-01 15:03:38 +0530

    Create Store context for golbal use use of db and add the remove and add functionality of cart

---
+++ Frontend/src/Context/StoreContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useEffect, useState } from 'react'
+import { food_list } from '../assets/assets';
+
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+
+    const [cartItem, setCartItem] = useState({});
+
+    const addToCart = (itemId) => {
+        if (!cartItem[itemId]) {
+            setCartItem((prev) => ({ ...prev, [itemId]: 1 }));
+        }
+        else {
+            setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    }
+
+    useEffect(() => {
+        console.log(cartItem)
+    }, [cartItem])
+    const contextValue = {
+        food_list,
+        cartItem,
+        setCartItem,
+        addToCart,
+        removeFromCart,
+    }
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+export default StoreContextProvider;
