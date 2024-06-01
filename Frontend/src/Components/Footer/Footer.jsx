4bf152c14efe6467872dfb4097dfe753c673fc92
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-01 15:04:00 +0530

    Footer component

---
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <>
+            <div id="footer" className="footer">
+                <div className="footer-content">
+                    <div className="footer-content-left">
+                        <img src={assets.logo} alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique natus fuga quidem ullam fugiat hic voluptate molestiae odit, recusandae possimus omnis veritatis reprehenderit explicabo nihil in eaque molestias obcaecati? Repellendus?</p>
+                        <div className="footer-social-icon">
+                            <img src={assets.facebook_icon} alt="" />
+                            <img src={assets.twitter_icon} alt="" />
+                            <img src={assets.linkedin_icon} alt="" />
+                        </div>
+                    </div>
+                    <div className="footer-content-center">
+                        <h2>COMPANY</h2>
+                        <ul>
+                            <li>Home</li>
+                            <li>About us</li>
+                            <li>Delivery</li>
+                            <li>Privacy Policy</li>
+                        </ul>
+                    </div>
+                    <div className="footer-content-right">
+                        <h2>GET IN TOUCH</h2>
+                        <ul>
+                            <li>+91-966722xxxx</li>
+                            <li>viahalyadav@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="divider"></div>
+                <p className="footer-copyright">Copyright 2024 @ Tomato.com - All Rights Reserved</p>
+            </div>
+        </>
+    )
+}
+
+export default Footer
+
