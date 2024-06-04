3b2f13000a1bc9dc032fe0c7ad10d3318ae6903e
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-04 12:20:47 +0530

    Use the navbar and footer component gloabally

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cart/Cart'
+import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
+import Login from './Pages/Login/Login'
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
   return (
-    <div>
-      Hlo my name is vishal
-    </div>
+    <>
+      <BrowserRouter>
+      {showLogin ? <Login setShowLogin={setShowLogin}/> : <></>}
+      <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
 }
 
--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,23 +1,17 @@
 import React, { useState } from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
 import Hero from '../../Components/Hero/Hero'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
-import Footer from '../../Components/Footer/Footer'
-import Login from '../Login/Login'
 
 const Home = () => {
     const [category, setCategory] = useState("All");
-    const [showLogin, setShowLogin] = useState(false);
+    
     return (
         <>
-            {showLogin ? <Login setShowLogin={setShowLogin}/> : <></>}
             <div className="app">
-                <Navbar setShowLogin={setShowLogin} />
                 <Hero />
                 <ExploreMenu category={category} setCategory={setCategory} />
                 <FoodDisplay category={category} />
-                <Footer />
             </div>
         </>
     )
