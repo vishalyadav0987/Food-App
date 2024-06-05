635a45a9867ae2702973a7ab834c6c587e2bb2e5
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-05 18:41:18 +0530

    Set multer to upload image to add new food in a site

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -3,10 +3,18 @@ const router = express.Router();
 const { addNewFood } = require('../controllers/foodController');
 const multer = require('multer');
 
+// IMAGE STORAGE ENGINE
+const storage = multer.diskStorage({
+    destination: "uploads",
+    filename: (req, file, cb) => {
+        return (cb, `${Date.now()}${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 
-
-router.post('/add',addNewFood);
+router.post('/add', upload.single("image"), addNewFood);
 
 
 module.exports = router;
