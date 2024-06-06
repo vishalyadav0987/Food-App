675858bb6b4216aee7946ac2421a71d26a914afb
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-06 12:42:13 +0530

    Add Logic of  getAllFoods function in foodcontroller

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -2,7 +2,7 @@ const FoodSchema = require('../modals/FoodSchema');
 
 
 // ADD NEW FOOD
-const addNewFood = async (req,res) => {
+const addNewFood = async (req, res) => {
     let image_filename = `${req.file.filename}`;
     const { name, description, price, category } = req.body;
     const newData = new FoodSchema({
@@ -21,6 +21,18 @@ const addNewFood = async (req,res) => {
     }
 }
 
+// GET ALL FOOD OR LOST OF FOODS
+const getAllFoods = async (req, res) => {
+    try {
+        const foods = await FoodSchema.find({});
+        res.json({ success: true, data: foods,message:"Food is Fetch Succesfully" });
+    } catch (error) {
+        console.log("Error in getAllFoods Function: ", error);
+        res.json({ success: false, message: "Food is not Fetch", error });
+    }
+}
+
 module.exports = {
     addNewFood,
+    getAllFoods,
 }
