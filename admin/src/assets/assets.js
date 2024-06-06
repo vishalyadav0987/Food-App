93ff30bc8bb5cd159b6c8487e8fe7f5c6b0dd6d8
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-07 00:17:16 +0530

    Add cross icon in assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,6 +4,7 @@ import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
+import cross_icon from './cross_icon.png'
 
 export const assets ={
     logo,
@@ -11,7 +12,8 @@ export const assets ={
     order_icon,
     profile_image,
     upload_area,
-    parcel_icon
+    parcel_icon,
+    cross_icon
 }
 
 export const url = 'http://localhost:4000'
