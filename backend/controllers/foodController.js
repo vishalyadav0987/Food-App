799f4ed6684a1120dc8b6300aa6c3e5cdc0646d7
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-06 12:56:30 +0530

    Add Logic of removeFood function in foodcontroller

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,5 +1,5 @@
 const FoodSchema = require('../modals/FoodSchema');
-
+const fs = require('fs');
 
 // ADD NEW FOOD
 const addNewFood = async (req, res) => {
@@ -25,14 +25,32 @@ const addNewFood = async (req, res) => {
 const getAllFoods = async (req, res) => {
     try {
         const foods = await FoodSchema.find({});
-        res.json({ success: true, data: foods,message:"Food is Fetch Succesfully" });
+        res.json({ success: true, data: foods, message: "Food is Fetch Succesfully" });
     } catch (error) {
         console.log("Error in getAllFoods Function: ", error);
         res.json({ success: false, message: "Food is not Fetch", error });
     }
 }
 
+
+// REMOVE FOOD ITEM
+const removeFood = async (req, res) => {
+    try {
+        // THIS ID FROM POST REQUEST // NOT FROM DELETE REQUEST
+        const { id } = req.body;
+        const food = await FoodSchema.findById(id);
+        console.log(food);
+        fs.unlink(`uploads/${food.image}`, () => { });
+        await FoodSchema.findByIdAndDelete(id);
+        res.json({ success: true, message: "Food is succesfully removed" });
+    } catch (error) {
+        console.log("Error in removeFood Function: ", error);
+        res.json({ success: false, message: "Food is not removed", error });
+    }
+}
+
 module.exports = {
     addNewFood,
     getAllFoods,
+    removeFood,
 }
