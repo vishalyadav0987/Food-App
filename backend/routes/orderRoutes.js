ea0826bfc822a489c8da73e5c3336bfaf095553b
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-08 19:19:18 +0530

    Set Status route using post method pass orderId and what should be the Status of the item

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,7 +4,8 @@ const protectedRouteMiddleware = require('../middlewares/protectedRoute')
 const { placeOrder,
     verifyOrder,
     userOrders,
-    getAllOrderForAdmin
+    getAllOrderForAdmin,
+    updateOrderStatus,
 } = require('../controllers/orderController');
 
 
@@ -12,6 +13,7 @@ router.post('/place', protectedRouteMiddleware, placeOrder);
 router.post('/verify', verifyOrder);
 router.post('/userorders', protectedRouteMiddleware, userOrders);
 router.get('/list', getAllOrderForAdmin);
+router.post('/status', updateOrderStatus);
 
 
 module.exports = router;
