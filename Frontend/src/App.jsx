b2bae9403cbbb28a323d0a040af71536831feaed
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-08 15:07:17 +0530

    Add the verify route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,20 +6,22 @@ import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
 import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
 import Login from './Pages/Login/Login'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
+import Verify from './Pages/Verify/Verify'
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
   return (
     <>
       <BrowserRouter>
-      {showLogin ? <Login setShowLogin={setShowLogin}/> : <></>}
-      <Toaster/>
-      <Navbar setShowLogin={setShowLogin} />
+        {showLogin ? <Login setShowLogin={setShowLogin} /> : <></>}
+        <Toaster />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
+          <Route path='/verify' element={<Verify />} />
         </Routes>
         <Footer />
       </BrowserRouter>
