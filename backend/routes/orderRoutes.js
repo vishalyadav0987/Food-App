e6a589523f9d0713eadfbcc416ba5cc041f0a297
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-08 16:32:30 +0530

    Set the userOrders routes using post method

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const protectedRouteMiddleware = require('../middlewares/protectedRoute')
-const { placeOrder,verifyOrder } = require('../controllers/orderController');
+const { placeOrder,verifyOrder,userOrders } = require('../controllers/orderController');
 
 
 router.post('/place',protectedRouteMiddleware,placeOrder);
-router.post('/verify',verifyOrder)
+router.post('/verify',verifyOrder);
+router.post('/userorders',protectedRouteMiddleware,userOrders);
 
 
 module.exports = router;
