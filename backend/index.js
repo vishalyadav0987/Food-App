592b57c460c45c345517dfd4516aa3c30170fb01
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-08 09:14:54 +0530

    Set the order endpoint

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const connectDB = require('./connectDB/connect');
 const foodRoutes = require('./routes/foodRoute');
 const userRoutes = require('./routes/userRoute');
 const cartRoutes = require('./routes/cartRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 app.use(express.json());
 app.use(cors());
@@ -16,6 +17,7 @@ app.use('/api/v1/food', foodRoutes);
 app.use('/images', express.static('./uploads')); // SHOWING IMAGE ON LOCALHOST LINK [http://localhost:3000/images/17176555682191705388450800.jpg]
 app.use('/api/v1/user',userRoutes);
 app.use('/api/v1/cart',cartRoutes);
+app.use('/api/v1/order',orderRoutes);
 
 app.get('/test', (req, res) => {
     res.send("Hi !! this is test route for testing purpose");
