8a5489fa46d9a62da72dd28f89da059a9d41978b
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-08 17:54:42 +0530

    Set route for the get all the order for admin panel using get method

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const protectedRouteMiddleware = require('../middlewares/protectedRoute')
-const { placeOrder,verifyOrder,userOrders } = require('../controllers/orderController');
+const { placeOrder,
+    verifyOrder,
+    userOrders,
+    getAllOrderForAdmin
+} = require('../controllers/orderController');
 
 
-router.post('/place',protectedRouteMiddleware,placeOrder);
-router.post('/verify',verifyOrder);
-router.post('/userorders',protectedRouteMiddleware,userOrders);
+router.post('/place', protectedRouteMiddleware, placeOrder);
+router.post('/verify', verifyOrder);
+router.post('/userorders', protectedRouteMiddleware, userOrders);
+router.get('/list', getAllOrderForAdmin);
 
 
 module.exports = router;
