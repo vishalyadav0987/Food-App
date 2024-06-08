2aeaba65d33266dd282887b489e96ae13631050f
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-08 19:21:42 +0530

    Required changes for ui download some dependencies

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './index.css'
+import Navbar from './Components/Navbar/Navbar'
+import SideBar from './Components/SideBar/SideBar'
+import { Routes, Route } from 'react-router-dom'
+import Add from './Pages/Add/Add'
+import List from "./Pages/ListGetallfood/List"
+import Order from './Pages/Orders/Order'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
     <>
-      
+      <Toaster />
+      <Navbar />
+      {/* <hr /> */}
+      <div className="app-component">
+        <SideBar />
+        <Routes>
+          <Route path='/add' element={<Add />} />
+          <Route path='/list' element={<List />} />
+          <Route path='/orders' element={<Order />} />
+        </Routes>
+      </div>
     </>
   )
 }
--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import {assets} from '../../assets/assets'
+import { assets } from '../../assets/assets'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className="navbar-container">
-        <img src={assets.logo} alt="" className="logo" />
-        <img src={assets.profile_image} alt="" className="admin-logo" />
+    <>
+      <div className='navbar'>
+        <div className="navbar-container">
+          <img src={assets.logo} alt="" className="logo" />
+          <img src={assets.profile_image} alt="" className="admin-logo" />
+        </div>
+        <hr />
       </div>
-    </div>
+    </>
   )
 }
 
