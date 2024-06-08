55149b11d8ae416009869017253fac5c6f085600
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-08 09:03:49 +0530

    create order schema for admin panel for taking order and address from the user

---
+++ backend/modals/OrderSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    items: {
+        type: Array,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    address: {
+        type: Object,
+        required: true,
+    },
+    status: {
+        type: String,
+        default: "Food Processing",
+    },
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
+    payment: {
+        type: Boolean,
+        default: false,
+    }
+});
+
+module.exports = mongoose.models.order || mongoose.model("order", OrderSchema);
