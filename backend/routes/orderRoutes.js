e30f54d39261097dabb2f4349fe77a99e4960431
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-06-08 15:02:54 +0530

    Set order verify route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const protectedRouteMiddleware = require('../middlewares/protectedRoute')
-const { placeOrder } = require('../controllers/orderController');
+const { placeOrder,verifyOrder } = require('../controllers/orderController');
 
 
 router.post('/place',protectedRouteMiddleware,placeOrder);
+router.post('/verify',verifyOrder)
 
 
 module.exports = router;
